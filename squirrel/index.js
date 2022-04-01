93111b90a205d70dc51c73ef286a6918eb268dea
Zayniddin <atabaevzayniddin2002@gmail.com>
2022-04-01 13:26:25 +0500

    One Line Task: Squirrel And Tree (JavaScript)

---
+++ squirrel/index.js
@@ -0,0 +1,5 @@
+squirrel=(h,H,S)=>(H*(S*S+h*h)**.5/h).toFixed(4)-0
+
+console.log(squirrel(4, 4, 3));
+// https://www.codewars.com/kata/reviews/59016bb7fe4001e3cf00059b/groups/6246b69abe56650001014013
+
