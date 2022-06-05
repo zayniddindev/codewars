cafdefe52ecda5f67f563c3512f193ee2128d0d5
Zayniddin Atabaev <atabaevzayniddin2002@gmail.com>
2022-06-05 10:07:55 +0500

    Vector problem

--- ts/dist/vector/index.js
+++ ts/dist/vector/index.js
@@ -16,15 +16,54 @@ class Vector {
     }
     //add
     add(vector) {
-        let res = [];
+        if (this.components.length !== vector.components.length)
+            throw new Error("Vectors must be equal");
+        else {
+            let res = [];
+            for (let i = 0; i < this.components.length; i++) {
+                const e = this.components[i];
+                res.push(e + vector.components[i]);
+            }
+            return new Vector(res);
+        }
+    }
+    //subtract
+    subtract(vector) {
+        if (this.components.length !== vector.components.length)
+            throw new Error("Vectors must be equal");
+        else {
+            let res = [];
+            for (let i = 0; i < this.components.length; i++) {
+                const e = this.components[i];
+                res.push(e - vector.components[i]);
+            }
+            return new Vector(res);
+        }
+    }
+    //dot
+    dot(vector) {
+        if (this.components.length !== vector.components.length)
+            throw new Error("Vectors must be equal");
+        else {
+            let res = [];
+            for (let i = 0; i < this.components.length; i++) {
+                const e = this.components[i];
+                res.push(e * vector.components[i]);
+            }
+            return res.reduce((a, b) => a + b);
+        }
+    }
+    //norm
+    norm() {
+        let sum = 0;
         for (let i = 0; i < this.components.length; i++) {
             const e = this.components[i];
-            res.push(e + vector[i]);
+            sum += e ** 2;
         }
-        return new Vector(res);
+        return Math.sqrt(sum);
     }
 }
 exports.Vector = Vector;
 let a = new Vector([1, 2, 3]);
-console.log(a.equals(new Vector([1, 2, 3])));
-console.log(a.toString());
+let b = new Vector([4, 5, 6]);
+console.log(a.dot(b));
--- ts/vector/index.ts
+++ ts/vector/index.ts
@@ -1,6 +1,6 @@
 // https://www.codewars.com/kata/526dad7f8c0eb5c4640000a4/train/typescript
 export class Vector {
-  private components: number[];
+  public components: number[];
   constructor(components: number[]) {
     this.components = components;
   }
@@ -13,16 +13,55 @@ export class Vector {
     return vector.components.join("") == this.components.join("");
   }
   //add
-  public add(vector: number[]) {
-    let res: number[] = [];
+  public add(vector: Vector) {
+    if (this.components.length !== vector.components.length)
+      throw new Error("Vectors must be equal");
+    else {
+      let res: number[] = [];
+      for (let i = 0; i < this.components.length; i++) {
+        const e = this.components[i];
+        res.push(e + vector.components[i]);
+      }
+      return new Vector(res);
+    }
+  }
+  //subtract
+  public subtract(vector: Vector) {
+    if (this.components.length !== vector.components.length)
+      throw new Error("Vectors must be equal");
+    else {
+      let res: number[] = [];
+      for (let i = 0; i < this.components.length; i++) {
+        const e = this.components[i];
+        res.push(e - vector.components[i]);
+      }
+      return new Vector(res);
+    }
+  }
+  //dot
+  public dot(vector: Vector) {
+    if (this.components.length !== vector.components.length)
+      throw new Error("Vectors must be equal");
+    else {
+      let res: number[] = [];
+      for (let i = 0; i < this.components.length; i++) {
+        const e = this.components[i];
+        res.push(e * vector.components[i]);
+      }
+      return res.reduce((a, b) => a + b);
+    }
+  }
+  //norm
+  public norm() {
+    let sum: number = 0;
     for (let i = 0; i < this.components.length; i++) {
       const e = this.components[i];
-      res.push(e + vector[i]);
+      sum += e ** 2;
     }
-    return new Vector(res);
+    return Math.sqrt(sum);
   }
 }
 
 let a = new Vector([1, 2, 3]);
-console.log(a.equals(new Vector([1, 2, 3])));
-console.log(a.toString());
+let b = new Vector([4, 5, 6]);
+console.log(a.dot(b));
